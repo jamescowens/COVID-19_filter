dadeb05080547817cce9b446d98933c44c2dde7e
jamescowens <jamesowens@optonline.net>
2020-03-31 10:40:18 -0400

    Improve run script.

--- COVID-19.sh
+++ COVID-19.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+source /home/jco/builds/COVID-19_filter/setexecenv_vars.dat
+
 cd /home/jco/builds/COVID-19
 git pull
+
 cd /home/jco/builds/COVID-19_web-data
 git pull
+
 cd /home/jco/builds/COVID-19_filter
-./COVID-19_filter 1>> filter_run.log 2>&1
+./COVID-19_filter 2>&1 | tee -a ./filter_run.log
